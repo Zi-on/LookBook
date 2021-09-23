508e2101b5e8e4bb83a0c05a3a9e9aa673d53859
Zi-on <zioniflores12@yahoo.com>
2021-09-23 02:50:40 -0500

    ...

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,7 +14,7 @@ const typeDefs = gql`
         authors: [String]
         description: String
         title: String
-        image: Image
+        image: String
         link: String
     }
 
@@ -30,7 +30,7 @@ const typeDefs = gql`
     type Mutation {
        login(email: String!, password: String!): Auth
        addUser(username: String!, email: String!, password: String!): Auth
-       saveBook(userId: ID, author: String!, description: String!, title: String!, image: Image!, link: String): User
+       saveBook(userId: ID, author: String!, description: String!, title: String!, image: String!, link: String): User
        removeBook(bookId: ID!): User
     }
 `
