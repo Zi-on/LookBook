41151d09aabad5f973ef886ece4440c625a20104
Zi-on <zioniflores12@yahoo.com>
2021-10-02 15:39:18 -0500

    fixed my me query and got books to populate the saved paged

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,7 @@ const { signToken } = require('../utils/auth');
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
-            console.log(args._id);
+            console.log(args, context.user);
             if(context.user) {
                 const userData = await User.findOne({_id: context.user._id})
                 .select('-__v -password')
@@ -13,7 +13,7 @@ const resolvers = {
 
             return userData;
             }
-            
+            throw new AuthenticationError("didnt work")
         },
     },
     Mutation: {
